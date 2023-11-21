a05ee0b461e82339be336086057eaa6a8cd1dea7
Usmonjon Qohhorov <100194299+UsmonjonQahhorov@users.noreply.github.com>
2023-11-21 15:38:24 +0500

    Update models.py

--- apps/orders/models.py
+++ apps/orders/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from apps.users.models import User
+
 from apps.orders.choices import OrderStatusChoice
 
 
@@ -10,26 +11,9 @@ class Order(models.Model):
     order_price = models.IntegerField()
     order_status = models.CharField(choices=OrderStatusChoice.choices, max_length=255)
 
-from apps.orders.choices import OrderStatusChoice
-from apps.users.models import User
-
-
-class Order(models.Model):
-    user_id = models.ManyToOneRel(
-        User,
-        related_name="user"
-    )
-    # service_id = models.ForeignKey(Serivice, on_delete=models.PROTECT)
-    order_date = models.DateTimeField(auto_now_add=True)
-    order_price = models.IntegerField()
-    order_status = models.CharField(choices=OrderStatusChoice.choices)
-
-
     class Meta:
         verbose_name = "order"
         verbose_name_plural = "orders"
 
     def __str__(self):
-         return str(self.order_date)
-        
-
+        return str(self.order_date)
