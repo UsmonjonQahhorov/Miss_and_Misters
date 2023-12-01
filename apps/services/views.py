affc0a270533c6f2bee85d0eeea73c858583965c
Usmonjon Qohhorov <qohhorovusmonjon@gmail.com>
2023-12-01 23:54:01 +0500

    master app updated

--- apps/services/views.py
+++ apps/services/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework.generics import CreateAPIView, DestroyAPIView,RetrieveAPIView,UpdateAPIView
+from apps.services.models import Services
+from apps.services.permissions import ServicePermision
+from apps.services.serializers import ServiceCreateSerializer
 
-# Create your views here.
+
+class ServiceCreateAPIView(CreateAPIView):
+    queryset = Services.objects.all()
+    serializer_class = ServiceCreateSerializer
+    permission_classes = [ServicePermision]
+
+
+class ServiceGetAPIView(RetrieveAPIView):
+    queryset = Services.objects.all()
+    serializer_class = ServiceCreateSerializer
+    permission_classes = [ServicePermision]
+
+
+class ServiceDeleteAPIView(DestroyAPIView):
+    queryset = Services.objects.all()
+    serializer_class = ServiceCreateSerializer
+    permission_classes = [ServicePermision]
+
+
+class ServiceUpdateAPIView(UpdateAPIView):
+    queryset = Services.objects.all()
+    serializer_class = ServiceCreateSerializer
+    permission_classes = [ServicePermision]
+
